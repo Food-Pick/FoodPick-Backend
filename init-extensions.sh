2150c3436e18094fc97dc1c7a7683a6b8b03acfe
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-05-11 17:51:04 +0900

    Create init-extensions.sh

---
+++ init-extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS system_stats;
+    CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+EOSQL 
