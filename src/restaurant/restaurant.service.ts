c9e34fec7b824a5e99fd28624bfaa05211831ac9
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-05-11 17:51:38 +0900

    Create restaurant.service.ts

---
+++ src/restaurant/restaurant.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RestaurantMerged } from '../entity/restaurant-merged.entity';
+
+@Injectable()
+export class RestaurantService {
+  constructor(
+    @InjectRepository(RestaurantMerged)
+    private restaurantRepository: Repository<RestaurantMerged>,
+  ) {}
+
+  async getRestaurants(): Promise<RestaurantMerged[]> {
+    return this.restaurantRepository.find({ take: 10 });
+  }
+
+  async findNearby(lat: number, lng: number, distance = 3000) {
+    const degreeRadius = distance / 111000;
+
+    return this.restaurantRepository
+      .createQueryBuilder('restaurant')
+      .addSelect(
+        `
+      ST_Distance(
+        restaurant.geom::geography,
+        ST_SetSRID(ST_MakePoint(:lng, :lat), 4326)::geography
+      )`,
+        'dist',
+      )
+      .where(
+        `
+      restaurant.geom && ST_Expand(ST_SetSRID(ST_MakePoint(:lng, :lat), 4326), :degreeRadius)
+      AND ST_DWithin(
+        restaurant.geom::geography,
+        ST_SetSRID(ST_MakePoint(:lng, :lat), 4326)::geography,
+        :distance
+      )
+    `,
+        { lat, lng, distance, degreeRadius },
+      )
+      .orderBy('dist', 'ASC')
+      .limit(50)
+      .getRawAndEntities();
+  }
+}
