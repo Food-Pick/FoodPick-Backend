a713f7a177c67fca8cd40c4a06bc106991bb1396
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-05-11 17:51:22 +0900

    Update app.module.ts
    TypeORM 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestaurantMerged } from './entity/restaurant-merged.entity';
+import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres', // docker-compose 서비스명
+      port: 5432,
+      username: 'foodpick',
+      password: 'foodpick123',
+      database: 'foodpick',
+      entities: [RestaurantMerged],
+      synchronize: false, // 실제 운영 DB라면 false 권장
+    }),
+    TypeOrmModule.forFeature([RestaurantMerged]),
+    RestaurantModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
