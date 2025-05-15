2e032d875ff40916e192284d8ea99deee1ab6381
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-05-15 18:05:43 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 # NestJS CLI가 없으면 설치
-if ! command -v nest > /dev/null; then
+if ! command -v nest > /dev/null;
+then
   npm install -g @nestjs/cli
 fi
 
 # package.json이 없으면 프로젝트 자동 생성
-if [ ! -f ./src/main.ts ] && [ ! -f ./package.json ]; then
+if [ ! -f ./src/main.ts ] && [ ! -f ./package.json ]; 
+then
   echo "NestJS 프로젝트가 없으므로 자동 생성합니다..."
   npx @nestjs/cli new . --package-manager npm
 fi
