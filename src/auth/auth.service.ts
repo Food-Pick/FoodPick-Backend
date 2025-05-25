aacb0a3056775c9d584bce4d4fbe51a0a7530aa4
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-05-25 16:42:27 +0900

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -16,28 +20,57 @@ export class AuthService {
 
   async login(loginDto: LoginDto) {
     const user = await this.authRepository.findOne({
-      where: { userId: loginDto.id }
+      where: { userId: loginDto.id },
     });
 
+    console.log(user);
+
     if (!user) {
-      throw new UnauthorizedException('아이디 또는 비밀번호가 일치하지 않습니다.');
+      throw new UnauthorizedException({
+        message: '아이디가 존재하지 않습니다.',
+        code: 'USER_NOT_FOUND',
+      });
     }
 
-    const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
     if (!isPasswordValid) {
-      throw new UnauthorizedException('아이디 또는 비밀번호가 일치하지 않습니다.');
+      throw new UnauthorizedException({
+        message: '비밀번호가 일치하지 않습니다.',
+        code: 'INVALID_PASSWORD',
+      });
     }
 
-    return { message: '로그인 성공' };
+    console.log('로그인 성공', user);
+
+    return {
+      message: '로그인 성공',
+      id: user.userId,
+      nickname: user.nickname,
+      email: user.email,
+      gender: user.gender,
+      age: user.age,
+      favorite_food: user.favorite_food,
+    };
   }
 
   async registerCheckId(idCheckDto: IdCheckDto) {
     const existingUser = await this.authRepository.findOne({
-      where: { userId: idCheckDto.id }
+      where: { userId: idCheckDto.id },
+    });
+
+    const existingUserByEmail = await this.authRepository.findOne({
+      where: { email: idCheckDto.email },
     });
 
     if (existingUser) {
       throw new ConflictException('이미 존재하는 아이디입니다.');
+    }
+
+    if (existingUserByEmail) {
+      throw new ConflictException('이미 존재하는 이메일입니다.');
     }
 
     return { message: '아이디 중복 확인 성공' };
@@ -45,20 +78,29 @@ export class AuthService {
 
   async register(registerDto: RegisterDto) {
     const existingUser = await this.authRepository.findOne({
-      where: { userId: registerDto.id }
+      where: { userId: registerDto.id },
+    });
+
+    const existingUserByEmail = await this.authRepository.findOne({
+      where: { email: registerDto.email },
     });
 
     if (existingUser) {
       throw new ConflictException('이미 존재하는 아이디입니다.');
     }
 
+    if (existingUserByEmail) {
+      throw new ConflictException('이미 존재하는 이메일입니다.');
+    }
+
     const hashedPassword = await bcrypt.hash(registerDto.password, 10);
     //보안을 위해 비밀번호를 해싱하여 저장
-    
+
     const user = this.authRepository.create({
       userId: registerDto.id,
       password: hashedPassword,
       nickname: registerDto.nickname,
+      email: registerDto.email,
       gender: registerDto.gender,
       age: registerDto.age,
       favorite_food: registerDto.favorite_food,
@@ -67,4 +109,4 @@ export class AuthService {
     await this.authRepository.save(user);
     return { message: '회원가입 성공' };
   }
-} 
+}
