905b3ee11450666a45d757540111055596185905
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-05-25 16:42:22 +0900

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,15 +8,22 @@ import { IdCheckDto } from './dto/id-check.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // @Post('login')
+  // async login(@Body() body: any) {
+  //   console.log('login 호출');
+  //   console.log(body);
+  // }
+
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
+    console.log('login 호출');
     console.log(loginDto);
     return this.authService.login(loginDto);
   }
 
   @Post('register-check-id')
   async registerCheckId(@Body() idCheckDto: IdCheckDto) {
-    console.log('register-check-id 호출');
+    console.log('register-check-id&email 호출');
     console.log(idCheckDto);
     return this.authService.registerCheckId(idCheckDto);
   }
@@ -27,4 +34,4 @@ export class AuthController {
     console.log(registerDto);
     return this.authService.register(registerDto);
   }
-} 
+}
