3718b9ed7674850f2ee84b19d0dcfc4c8641a495
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-05-25 16:42:41 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-    enableDebugMessages: true,
-    validationError: {
-      target: true,
-      value: true,
-    },
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      enableDebugMessages: true,
+      validationError: {
+        target: true,
+        value: true,
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
